ae487d921aaff6009763c6defb3f58861cc0a5c5
Ігор Люклян <ihor.liuklian.23@pnu.edu.ua>
2025-11-14 19:57:47 +0100

    Lab2: невеликий сайт із двох сторінок: резюме та контакти

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask # type: ignore
+from flask import Flask, render_template, request, url_for # type: ignore
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return 'Hello, world!'
+def index():
+    return render_template('resume.html', title='Резюме')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title='Резюме')
+
+@app.route('/contacts', methods=['GET', 'POST'])
+def contacts():
+    submitted = False
+    name = None
+    if request.method == 'POST':
+        name = request.form.get('name')
+        submitted = True
+    return render_template('contacts.html', title='Контакти', submitted=submitted, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
