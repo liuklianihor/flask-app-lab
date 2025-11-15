81653e298f10e8fd3f4a9b367433750d62e50dc2
Ігор Люклян <ihor.liuklian.23@pnu.edu.ua>
2025-11-15 15:53:25 +0100

    Lab3: контрольний commit: Unit Tests (test_product_bp.py, test_user_bp.py)

---
+++ tests/test_product_bp.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+
+class FlaskAppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.client = app.test_client()
+
+    def test_products_index(self):
+        response = self.client.get("/products/")
+        self.assertEqual(response.status_code, 200)
+        data = response.get_data(as_text=True)
+        self.assertIn("Goods", data)
+
+    def test_product_item(self):
+        response = self.client.get("/products/item/1")
+        self.assertEqual(response.status_code, 200)
+        data = response.get_data(as_text=True)
+        self.assertIn("Продукт #1", data)
+
+if __name__ == "__main__":
+    unittest.main()
