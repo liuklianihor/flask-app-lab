66f265f8f2b7885d453780aad9776ea58454bff4
Ігор Люклян <ihor.liuklian.23@pnu.edu.ua>
2025-11-19 13:17:43 +0100

    Lab4: нова структура проекту, блюпринт auth, реєстрація auth блюпринту в app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.secret_key = "my-secret-key"
 # app.config.from_pyfile("../config.py")
 
 
@@ -12,3 +13,6 @@ app.register_blueprint(post_bp, url_prefix='/users')
 
 from .products.views import products_bp
 app.register_blueprint(products_bp, url_prefix='/products')
+
+from .auth.views import auth_bp
+app.register_blueprint(auth_bp, url_prefix="/auth")
